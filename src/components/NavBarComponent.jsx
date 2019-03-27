a21d239672b6db1d600fb7fd2b8b4e5f00b964cb
forgeno <ima@ualberta.ca>
2019-03-27 10:22:23 -0600

    added redirect file

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -18,18 +18,18 @@ class NavBarComponent extends Component {
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand><h2>Ivan Ma</h2></Navbar.Brand>
                 <Nav className="mr-auto">
-                    <NavLink className="nav-link" to="/">Profile</NavLink>
+                    <NavLink className="nav-link" to="/home">Profile</NavLink>
                     <NavLink className="nav-link" to="/photos">Photography</NavLink>
                     <NavLink className="nav-link" to="/projects">Projects</NavLink>
                     <NavLink className="nav-link" to="/skills">Skills</NavLink>
                 </Nav>
             </Navbar>
             <Switch>
-            <Route exact path="/" component={HomePage}></Route>
+            <Route exact path="/home" component={HomePage}></Route>
             <Route exact path="/photos" component={PhotoPage}></Route>
             <Route exact path="/projects" component={ProjectPage}></Route>
             <Route exact path="/skills" component={SkillsPage}></Route>
-            <Route path="*" component={HomePage}><Redirect to="/"/></Route>
+            <Route path="*" component={HomePage}><Redirect to="/home"/></Route>
           </Switch>
         </BrowserRouter>
         </div>
