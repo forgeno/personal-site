856556e2f7727269176c017fc814c444c31940c3
forgeno <ima@ualberta.ca>
2019-03-27 02:09:43 -0600

    fixed redirect issue

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -3,7 +3,7 @@ import { Navbar,Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/NavBar.css';
 import { NavLink } from "react-router-dom";
-import { Route,BrowserRouter,Switch } from "react-router-dom";
+import { Redirect,Route,BrowserRouter,Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import PhotoPage from "../pages/PhotoPage";
 import ProjectPage from "../pages/ProjectPage";
@@ -29,7 +29,7 @@ class NavBarComponent extends Component {
             <Route exact path="/photos" component={PhotoPage}></Route>
             <Route exact path="/projects" component={ProjectPage}></Route>
             <Route exact path="/skills" component={SkillsPage}></Route>
-            <Route component={NotFound}></Route>
+            <Route exact path="/"><Redirect to="/home"/></Route>
           </Switch>
         </BrowserRouter>
         </div>
