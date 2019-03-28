6a415506c43dca97da795b6b86a0bd428336762b
forgeno <ima@ualberta.ca>
2019-03-27 21:43:17 -0600

    Moved navbar outside of component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import NavBarComponent from "./components/NavBarComponent";
+import { Redirect,Route,BrowserRouter,Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PhotoPage from "./pages/PhotoPage";
+import ProjectPage from "./pages/ProjectPage";
+import SkillsPage from './pages/SkillsPage';
 
 class App extends Component {
   render() {
     return (
       <div id="root">
-        <NavBarComponent/>
+        <BrowserRouter>
+          <NavBarComponent/>
+          <Switch>
+            <Route exact path="/home" component={HomePage}></Route>
+            <Route exact path="/photos" component={PhotoPage}></Route>
+            <Route exact path="/projects" component={ProjectPage}></Route>
+            <Route exact path="/skills" component={SkillsPage}></Route>
+            <Route path="*" component={HomePage}><Redirect to="/home"/></Route>
+          </Switch>
+        </BrowserRouter>  
       </div>
     );
   }
--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -3,35 +3,22 @@ import { Navbar,Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/NavBar.css';
 import { NavLink } from "react-router-dom";
-import { Redirect,Route,BrowserRouter,Switch } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import PhotoPage from "../pages/PhotoPage";
-import ProjectPage from "../pages/ProjectPage";
-import SkillsPage from '../pages/SkillsPage';
+
 
 class NavBarComponent extends Component {
 
     render() {
     return (
         <div className="navBar">
-        <BrowserRouter>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand><h2>Ivan Ma</h2></Navbar.Brand>
-                <Nav className="mr-auto">
-                    <NavLink className="nav-link" to="/home">Profile</NavLink>
-                    <NavLink className="nav-link" to="/photos">Photography</NavLink>
-                    <NavLink className="nav-link" to="/projects">Projects</NavLink>
-                    <NavLink className="nav-link" to="/skills">Skills</NavLink>
-                </Nav>
-            </Navbar>
-            <Switch>
-            <Route exact path="/home" component={HomePage}></Route>
-            <Route exact path="/photos" component={PhotoPage}></Route>
-            <Route exact path="/projects" component={ProjectPage}></Route>
-            <Route exact path="/skills" component={SkillsPage}></Route>
-            <Route path="*" component={HomePage}><Redirect to="/home"/></Route>
-          </Switch>
-        </BrowserRouter>
+          <Navbar bg="dark" variant="dark">
+              <Navbar.Brand><h2>Ivan Ma</h2></Navbar.Brand>
+              <Nav className="mr-auto">
+                  <NavLink className="nav-link" to="/home">Profile</NavLink>
+                  <NavLink className="nav-link" to="/photos">Photography</NavLink>
+                  <NavLink className="nav-link" to="/projects">Projects</NavLink>
+                  <NavLink className="nav-link" to="/skills">Skills</NavLink>
+              </Nav>
+          </Navbar>
         </div>
     );
   }
