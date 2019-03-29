d8d53c7939cf46974cf3a7d371ebd169951b7604
forgeno <ima@ualberta.ca>
2019-03-29 08:44:19 -0600

    attempt to fix redirect 2

--- src/index.js
+++ src/index.js
@@ -4,27 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import NavBarComponent from "./components/NavBarComponent";
-import { Redirect,Route,BrowserRouter,Switch } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import PhotoPage from "./pages/PhotoPage";
-import ProjectPage from "./pages/ProjectPage";
-import SkillsPage from './pages/SkillsPage';
-
-ReactDOM.render(
-    // <App />
-    <BrowserRouter>
-        <NavBarComponent/>
-        <Switch>
-            <Route exact path="/home" component={HomePage}></Route>
-            <Route exact path="/photos" component={PhotoPage}></Route>
-            <Route exact path="/projects" component={ProjectPage}></Route>
-            <Route exact path="/skills" component={SkillsPage}></Route>
-            <Route path="*" component={HomePage}><Redirect to="/home"/></Route>
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById('root')
-    );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
