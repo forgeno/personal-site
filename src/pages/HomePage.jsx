ef8c4edd28d14d893e422542825c176ef2ab50e2
forgeno <ima@ualberta.ca>
2019-04-02 14:16:06 -0600

    completed first version of photos page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./style/HomePage.css";
 import { withRouter } from 'react-router-dom';
 import { Image, Button } from "react-bootstrap";
-import headShot from "../assets/images/Headshot1.jpg"
+import headShot from "../assets/images/Headshot1Zoom.jpg"
 import linkedinIcon from "../assets/images/linkedin-white.png"
 import emailIcon from "../assets/images/mail-white.png"
 import githubIcon from "../assets/images/github-white.png"
@@ -46,25 +46,26 @@ class HomePage extends Component {
   render() {
     return (
     <div id="mainDiv">
+      <div id="contactButtons" className="animated slideInLeft slower">
+        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/ivanma0"><Image className="contactIcon" src={linkedinIcon}/></a>
+        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="https://github.com/forgeno/"><Image className="contactIcon" src={githubIcon}/></a>
+        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="mailto:ima@ualberta.ca"><Image className="contactIcon" src={emailIcon}/></a>
+      </div>
       <div id="info">
         <h1 className={this.state.visable? "animated slideInUp slow":"animated slideOutDown slow"}>Hi, My name is Ivan</h1>
-        <p className="animated slideInDown slow">
-        I'm a student at the university of alberta currently completing my program in Computing Science.
-
+        <p className="animated slideInDown slow" id="mainIntro">
+          Since I was little, I remember the dazzling lights and buzzing sounds coming from my
+          computer intriguing me. Thankfully my career aspirations are still filled with curiosity and
+          excitement. While I am still enthusiastic about pursuing different projects and learning as I go,
+          I found my interest in mobile/desktop all in one solutions the most interesting. 
         </p>
         <div id="buttonDiv">
           <Button className="animated fadeIn delay-1s" onClick={this.handleRedirectProjects} variant="success" size="sm">My Projects</Button>
           <Button className="animated fadeIn delay-1s" onClick={this.handleRedirectSkills} variant="info" size="sm">Skills</Button>
         </div>
       </div>
-      <div id="headshotDiv" className="animated fadeIn slower">
-        <Image id="headshotImage" src={headShot} rounded/>
-      </div>
-      <div id="contactButtons" className="animated fadeIn slow">
-        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/ivanma0"><Image className="contactIcon" src={linkedinIcon}/></a>
-        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="https://github.com/forgeno/"><Image className="contactIcon" src={githubIcon}/></a>
-        <a className="iconLink" target='_blank' rel="noopener noreferrer" href="mailto:ima@ualberta.ca"><Image className="contactIcon" src={emailIcon}/></a>
-      </div>
+      <Image id="headshotImage" className="animated fadeIn slower" src={headShot} rounded/>
+      
     </div>
     );
   }
--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import "./style/PhotoPage.css"
+import { Image,Container,Row,Col } from "react-bootstrap"
+import Building1 from "../assets/images/portfolio/Building1-square.png"
+import Seveteen from "../assets/images/portfolio/apricity-seventeen.png"
+import Legislature from "../assets/images/portfolio/Legislature-edmonton-square.png"
+import CalvinHeadshot from "../assets/images/portfolio/calvin-headshot-square.png"
+import MikeSquare from "../assets/images/portfolio/Mike-square.png"
+import DeiveSquare from "../assets/images/portfolio/IMG_2442.jpg"
+import "mdbootstrap/css/mdb.min.css"
 
 class PhotoPage extends Component {
 
@@ -9,10 +17,53 @@ class PhotoPage extends Component {
     };
   }
 
+  renderImageGrid(){
+    return(
+      <Container>
+        <Row>
+          <Col className="gridElement">
+            <div id="gridHeader">
+              <h1 className="animated fadeInDown slow">Lights, Camera, Action</h1>
+              <br/>
+              <p className="animated fadeInDown slow">
+                Photography is one of my biggest hobbies outside of work. Capturing the perfect moment in time and freezing it.
+                I've taken on small to medium sized events (~100 people) as well as one on one headshots. Dance and urban
+                photography are my specialties. Feel free to get in touch with me for future work.
+              </p>
+            </div>
+          </Col>
+          <Col className="gridElement">
+            <Image className="gridImage" src={Building1} ></Image>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="gridElement">
+            <Image className="gridImage" src={Seveteen}></Image>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="gridElement">
+            <Image className="gridImage" src={Legislature}></Image>
+          </Col>
+          <Col className="gridElement">
+            <Image className="gridImage" src={CalvinHeadshot}></Image>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="gridElement">
+            <Image className="gridImage wow fadeInUp" src={MikeSquare}></Image>
+          </Col>
+          <Col className="gridElement wow fadeInUp">
+            <Image className="gridImage" src={DeiveSquare}></Image>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
   render() {
     return (
         <div>
-            <h1>Photos Page</h1>
+            {this.renderImageGrid()}
         </div>
     );
   }
